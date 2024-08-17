70e22e09d511f295947e7655052fd1d6d6a381ae
DanStew <danielstewarts2004@gmail.com>
2024-08-17 04:38:11 +0100

    Finished CLI, Removing functions into their own file

--- roleEvaluatorFunctions.py
+++ roleEvaluatorFunctions.py
@@ -1,8 +1,8 @@
 import glob
 import os
 import sys
-from tabulate import tabulate
 import uuid
+import tabulate
 import webbrowser
 
 # Function to find and read the latest file from FMData
@@ -635,46 +635,3 @@ def resetAttributeMultipliers():
         print("Exiting out of System...")
         sys.exit()
     print("Attribute Rankings successfully set to default values")
-
-#Start Point of the Program, CLI Menu
-def mainFunction():
-    while True:
-        #Outputting options to the user
-        print()
-        print("===============================================================")
-        print("      Welcome to Football Manager Player Analysis")
-        print()
-        print("Please input a number from the options below : ")
-        print("1. Generate and Output Player Scores")
-        print("2. Update Attribute Ranking Variables")
-        print("3. Reset Attribute Rankings")
-        print("4. Exit Program")
-        #Collecting the input from the user
-        nmb = input("Enter your choice (1 to 4) : ")
-        if (nmb!="1" and nmb!="2" and nmb!="3" and nmb!="4"):
-            print("Input must be a whole number between 1 and 4")
-            print()
-        #Transferring user to the location they want
-        if (nmb == "1"):
-            print("Transferring you to the Player Role Generator System...")
-            print()
-            outputPlayerScores()
-            print()
-        if (nmb == "2"):
-            print("Transferring you to the Attribute Updater System...")
-            print()
-            updateAttributeRankings()
-            print()
-        if (nmb == "3"):
-            print("Transferring you to the Reset Attribute Multipliers System...")
-            print()
-            ensureUserResponse()
-            print()
-        if (nmb == "4"):
-            print("PROGRAM EXITING...")
-            sys.exit()
-
-#Calling the start point
-if __name__ == "__main__":
-    mainFunction()
-
